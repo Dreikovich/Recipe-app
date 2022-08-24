9c8ef67f6865002da74aea33683784ee8c37cf2d
drewkevich <AndrzejHK@gmail.com>
2022-08-24 17:14:30 +0200

    Changes in design of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,42 +10,54 @@ const Footer = () => {
   return (
     <Box sx={{ backgroundColor:'#2E3B55', marginTop: '30px', color:'#FFFFFF' }}>
       <Container maxWidth="lg" >
-        <Grid container spacing={3}>
-          <Grid item xs={6} sm={6}>
-            <Box sx={{borderBottom:"1px solid #FFFFFF"}}>Contact</Box>
-            <Box sx={{marginTop: '15px', display: 'flex'}}>
-              <EmailIcon/>
-              <Link href="mailto:andrzejhk@gmail.com" style={{marginLeft:"5px"}} color="inherit">
-              andrzejhk@gmail.com
-              </Link>
+        <Grid container spacing={1}  direction="row" justifyContent="center"   >
+          <Grid item xs={6} sm={6} md={6}  >
+            <Box sx={{display: 'flex', flexDirection:'column', alignItems: 'center'}}>
+              <Box component='h3' sx={{borderBottom:"1px solid #FFFFFF"}}>Contact</Box>
+                <Box sx={{marginTop: '15px', display: 'flex', flexDirection: 'column'}}>
+                  <Box sx={{ display: 'flex'}}>
+                    <EmailIcon/>
+                    <Link href="mailto:andrzejhk@gmail.com" style={{marginLeft:"5px"}} color="inherit">
+                    andrzejhk@gmail.com
+                    </Link>
+                  </Box>
+                  <Box sx={{marginTop: '5px', display: 'flex'}}>
+                    <GitHubIcon/>
+                    <Link href="https://github.com/drewkevich" style={{marginLeft:"5px"}} color="inherit">
+                      Github
+                    </Link>
+                  </Box>
+                  <Box sx={{marginTop: '5px', display: 'flex'}}>
+                    <PhoneIcon />
+                    <Typography style={{marginLeft:"5px"}} color="inherit">
+                      +48577776746
+                    </Typography>
+                  </Box>
+                  <Box sx={{marginTop: '5px', display: 'flex', marginBottom: '20px'}}>
+                    <LocationOnIcon />
+                    <Typography style={{marginLeft:"5px"}} color="inherit">
+                      Warsaw
+                    </Typography>
+                  </Box>
+              </Box>
+             
             </Box>
-            <Box sx={{marginTop: '5px', display: 'flex'}}>
-              <GitHubIcon/>
-              <Link href="https://github.com/drewkevich" style={{marginLeft:"5px"}} color="inherit">
-                Github
-              </Link>
-            </Box>
-            <Box sx={{marginTop: '5px', display: 'flex'}}>
-              <PhoneIcon />
-              <Typography style={{marginLeft:"5px"}} color="inherit">
-                +48577776746
-              </Typography>
-            </Box>
-            <Box sx={{marginTop: '5px', display: 'flex', marginBottom: '20px'}}>
-              <LocationOnIcon />
-              <Typography style={{marginLeft:"5px"}} color="inherit">
-                Warsaw
-              </Typography>
-            </Box>
+            
           </Grid>
-          <Grid item xs={6} sm={6}>
-            <Box sx={{borderBottom:"1px solid #FFFFFF"}}>The site was created using</Box>
-            <Box sx={{marginTop: '15px', display: 'flex', flexDirection: 'column'}}>
-              <Typography style={{marginTop: '3px'}}>React </Typography>
-              <Typography style={{marginTop: '3px'}}>Material-UI</Typography>
-              <Typography style={{marginTop: '3px'}}>TheMealDB Api</Typography>
-              <Typography style={{marginTop: '3px'}}>Pixabay Api</Typography>
+          <Grid item xs={6} sm={6} md={6}>
+            <Box sx={{display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems: 'center'}}>
+              <Box component='h3' sx={{borderBottom:"1px solid #FFFFFF"}}>Created using</Box>
+              <Box sx={{marginTop: '15px', display: 'flex', flexDirection: 'column'}}>
+                <Box>
+                  <Typography>React </Typography>
+                </Box>
+                
+                <Typography style={{marginTop: '5px'}}>Material-UI</Typography>
+                <Typography style={{marginTop: '5px'}}>TheMealDB Api</Typography>
+                <Typography style={{marginTop: '5px'}}>Pixabay Api</Typography>
+              </Box>
             </Box>
+            
           </Grid>
 
         </Grid>
